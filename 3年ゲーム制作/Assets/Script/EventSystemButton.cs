c0e89f292afb7560a06c6b58cd9e0d112d1cacc1
koitabashi0222 <koita0222yuh@icloud.com>
2024-07-09 14:48:33 +0900

    進捗
    ボタン押し込めるように
    チーズの力の入れ方調整etc

--- 3年ゲーム制作/Assets/Script/EventSystemButton.cs
+++ 3年ゲーム制作/Assets/Script/EventSystemButton.cs
@@ -8,10 +8,17 @@ public class EventSystemButton : MonoBehaviour
     [SerializeField] Material defaultButtonMaterial;
     [SerializeField] Material pushedButtonMaterial;
     [SerializeField] UnityEvent onButtonPressed;
+
+    [SerializeField] private float buttonPressDepth = 0.1f;//ボタンが押し下げられる深さ
+    [SerializeField] private float buttonPressDuration = 0.2f;//ボタンが押し下げられる時間
+
+    private Vector2 originalPosition;
+    private bool isPressed = false;
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Renderer>().material = defaultButtonMaterial;
+        originalPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -24,11 +31,27 @@ public class EventSystemButton : MonoBehaviour
         if (collision.gameObject.tag == "cheese")
         {
             CheeseController cheese = collision.gameObject.GetComponent<CheeseController>();
-            if (cheese != null && !cheese.IsHeld)
+            if (cheese != null && !cheese.IsHeld&&!isPressed)
             {
+                isPressed = true;
                 GetComponent<Renderer>().material = pushedButtonMaterial;
                 onButtonPressed.Invoke();
+                StartCoroutine(PressButton());
             }
         }
+    }
+
+    private IEnumerator PressButton()
+    {
+        Vector2 targetposition=originalPosition-new Vector2(0,buttonPressDepth);
+        float elapsedtime = 0;
+
+        while (elapsedtime<buttonPressDuration)
+        {
+            transform.position=Vector2.Lerp(originalPosition,targetposition,elapsedtime/buttonPressDuration);
+            elapsedtime+=Time.deltaTime;
+            yield return null;
+        }
+        transform.position=targetposition;
     }
 }
