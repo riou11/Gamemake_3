fb05e87c2b6c0ab74949cecd8eb07541abd1326c
koitabashi0222 <koita0222yuh@icloud.com>
2024-07-17 19:14:58 +0900

    チーズ関係の処理統合
    プルリクエストがこっちのブランチでCheeseControllerのスクリプトいじってあってうまくいかなかったから直接スクリプトを入れた

---
+++ 3年ゲーム制作/Assets/Script/CheeseManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject cheesePrefab;
+    public GameObject cheeseHolder;
+    public int lives = 3;
+
+    private GameObject currentCheese;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HoldNewCheese();
+    }
+
+    public void LoseCheese()
+    {
+        if (currentCheese != null)
+        {
+            Destroy(currentCheese);
+        }
+
+        lives--;
+        if (lives > 0)
+        {
+            Invoke("HoldNewCheese", 0.5f);
+        }
+        else
+        {
+            Debug.Log("Game Over");
+        }
+    }
+
+    public void HoldNewCheese()
+    {
+        if (cheesePrefab != null && cheeseHolder != null)
+        {
+            currentCheese = Instantiate(cheesePrefab, cheeseHolder.transform.position, Quaternion.identity);
+            CheeseController cheeseController = currentCheese.GetComponent<CheeseController>();
+            cheeseController.Initialize(player, cheeseHolder, this);
+        }
+        else
+        {
+            Debug.LogError("CheesePrefab or CheeseHolder is not set.");
+        }
+    }
+
+}
