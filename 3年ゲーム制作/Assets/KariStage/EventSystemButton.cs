dbca6578dd62a1a39c15c487ebe9c197bbdb84c6
koitabashi0222 <koita0222yuh@icloud.com>
2024-10-01 11:12:37 +0900

    commit

--- 3年ゲーム制作/Assets/KariStage/EventSystemButton.cs
+++ 3年ゲーム制作/Assets/KariStage/EventSystemButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class kariEventSystemButton : MonoBehaviour
+public class EventSystemButton : MonoBehaviour
 {
     [Header("ƒvƒŒƒCƒ„[‚Ì”»’è")] public PlayerTriggerCheck playerCheck;
     [SerializeField] Material defaultButtonMaterial;
