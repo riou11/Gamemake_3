7fddbbc4858550ba78c41e42411cbd39db2b8577
koitabashi0222 <koita0222yuh@icloud.com>
2024-10-08 14:44:11 +0900

    10/8進捗

---
+++ 3年ゲーム制作/Assets/KariStage/Script/CatMove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMove : MonoBehaviour
+{
+    public GameObject player;
+    public int speed;
+    private bool isStopped = false;
+    public StageCtrl stageCtrl; // StageCtrlへの参照
+    private Animator anim = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isStopped)
+        {
+            // プレイヤー-敵キャラの位置関係から方向を取得し、速度を一定化
+            Vector2 targeting = (player.transform.position - this.transform.position).normalized;
+            if (targeting.x > 0)
+            {
+                this.GetComponent<SpriteRenderer>().flipX = false;
+                anim.SetBool("run", true);
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().flipX = true;
+                anim.SetBool("run", true);
+            }
+            // x方向にのみプレイヤーを追う
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2((targeting.x * speed), 0);
+        }
+        else
+        {
+            // 停止中は速度をゼロにする
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            anim.SetBool("run", false);
+        }
+    }
+
+    public void StopChasing(float duration)
+    {
+        StartCoroutine(StopChasingCoroutine(duration));
+    }
+
+    private IEnumerator StopChasingCoroutine(float duration)
+    {
+        isStopped = true;
+        yield return new WaitForSeconds(duration);
+        isStopped = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            stageCtrl.OnEnemyCollected();
+        }
+    }
+}
