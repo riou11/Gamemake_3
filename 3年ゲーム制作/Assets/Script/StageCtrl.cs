60db180fbe0504d68aef9a194f1b930a7099aed1
sG468 <90618981+sG468@users.noreply.github.com>
2024-12-02 21:37:30 +0900

    resultShows
    I pushed result Shows.

--- 3年ゲーム制作/Assets/Script/StageCtrl.cs
+++ 3年ゲーム制作/Assets/Script/StageCtrl.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 //インゲーム中の、進行度等に応じたUI遷移の管理クラス
 
 public class StageCtrl : MonoBehaviour
 {
+    GameManager gameManager => GameManager.Instance;
+
     [Header("チーズパラメーターUI")]
     [SerializeField] public Image cheeseParameters;
     [Header("体力ゲージUI")]
@@ -26,27 +29,61 @@ public class StageCtrl : MonoBehaviour
     [Header("猫のダメージ絵")]//rio
     public GameObject CatDamageObj;
 
+    /// <summary>
+    /// リザルト画面UI
+    /// </summary>
+    //[Header("GameClearのロゴ")]
+    //[SerializeField] private GameObject _clearText;
+    [Header("「Press Any Key」の画像")]
+    [SerializeField] private GameObject _messageText;
+    //[Header("リザルト画面のネズミの画像")]
+    //[SerializeField] private GameObject[] _ratImgObjs;
+    [Header("リザルト画面のネズミと吹き出しと星の画像")]
+    [SerializeField] private GameObject[] _resultImgObjs;
+
+    /// <summary>
+    /// リザルト画面の次の選択画面UI
+    /// </summary>
+    [Header("遷移選択画面")]
+    [SerializeField] private GameObject _transSelectionUI;
+    //[Header("Retryボタン")]
+    //[SerializeField] private Button _retryButton;
+
+    /// <summary>
+    /// ボタン関係
+    /// </summary>
+    [Header("Retryボタン")]
+    [SerializeField] private GameObject _retryButton;
+    [Header("NextStageボタン")]
+    [SerializeField] private GameObject _nextStageButton;
+    //[Header("失敗時のBackToTitleボタン")]
+    //[SerializeField] private GameObject _gmBackToTitleButton;
+    //[Header("Clear時のBackToTitleボタン")]
+    //[SerializeField] private GameObject _gcBackToTitleButton;
+
+    
     public bool doGameOver = false;
     public bool doGameClear = false;
     private bool retryGame = false;
     private int nextStageNum;
 
-    GameManager gameManager;
+    private float _evalution = 0f; //（獲得チーズ数 / そのステージの上限チーズ数）の計算結果
+    private int _value = 0; //評価値計算過程の計算結果格納用（評価値の査定を、整数値で行いたいためint型）
+    private int _result = 0; //そのステージの評価値（星の数）、保存はされない
+
 
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1.0f;
 
-        gameManager = FindObjectOfType<GameManager>();
+        //gameManager = FindObjectOfType<GameManager>();
 
         if (playerObj != null && gameOverObj != null && stageClrObj != null && InGameUIObj != null)
         {
-            gameOverObj.SetActive(false);
-            stageClrObj.SetActive(false);
-            CatDamageObj.SetActive(false);
-            InGameUIObj.SetActive(true);
+            UIImgSetUp();
             doGameOver = false;
+            doGameClear = false;
         }
         else
         {
@@ -57,7 +94,20 @@ public class StageCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (doGameClear)
+        {
+            TransSelection();
+        }
+    }
 
+    //ステージ開始時のUIセットアップ
+    void UIImgSetUp()
+    {
+        _transSelectionUI.SetActive(false);
+        gameOverObj.SetActive(false);
+        stageClrObj.SetActive(false);
+        CatDamageObj.SetActive(false);
+        InGameUIObj.SetActive(true);
     }
 
     //爆弾チーズを獲ってしまった時のゲームオーバー処理
@@ -100,13 +150,21 @@ public class StageCtrl : MonoBehaviour
         yield return new WaitForSeconds(2.0f); // 2秒待つ（必要に応じて変更）
 
         gameOverObj.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_retryButton);
+
         doGameOver = true;
         Time.timeScale = 0f;
     }
 
+    public void BackToTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
     public void Retry()
     {
-        SceneManager.LoadScene("CatMoveTest");
+        SceneManager.LoadScene(gameManager.CurrentStage().ToString());
     }
 
     public void Retry0()
@@ -133,10 +191,95 @@ public class StageCtrl : MonoBehaviour
         SceneManager.LoadScene(nextStageNum); // シーンを変更する
     }
 
+    //ゴールに着いた時に呼び出す処理
     public void arrivedGoal()
     {
+        doGameClear = true;
         stageClrObj.SetActive(true);
+        InGameUIObj.SetActive(false);
+        StartCoroutine(ClearEvent());
+    }
+
+    //リザルト画面周りの処理
+    IEnumerator ClearEvent()
+    {
+        //yield return new WaitForSeconds(0.5f);
+        //_clearText.SetActive(true);
+        _result = CalcEvalution(gameManager.cheeseScore);
+
+        //yield return new WaitForSeconds(0.5f);
+
+        //_ratImgObjs[_result].SetActive(true);
+        _resultImgObjs[_result].SetActive(true);
+
+        yield return new WaitForSeconds(1.0f);
+
+        _messageText.SetActive(true);
+
         Time.timeScale = 0f;
+    }
+
+    int CalcEvalution(int score)
+    {
+        _evalution = 0f;
+
+        _evalution = score / gameManager.RecentCheeseLimit();
+
+        _value = (int)(_evalution * 10);
+
+        if (_value <= 4)
+        {
+            return 0;
+        }
+        else if ((_value > 4) && (_value <= 8))
+        {
+            return 1;
+        }
+        else if (_value > 8)
+        {
+            return 2;
+        }
+        else
+        {
+            Debug.Log("calculation didn't go well");
+            return 0;
+        }
+    }
+
+    //「Press Any Key」が表示された後の処理
+    void TransSelection()
+    {
+        //「Press Any Key」と表示されたら
+        if (_messageText.activeSelf)
+        {
+            if (Input.anyKey)
+            {
+                ResultImgPassive();
+                TransSelecImgActive();
+            }
+        }
+        else if (_transSelectionUI.activeSelf) //遷移選択画面に飛んだら
+        {
+
+        }
+    }
+    
+    //リザルト画面の非表示
+    void ResultImgPassive()
+    {
+        //_clearText.SetActive(false);
+        //_ratImgObjs[_result].SetActive(false);
+        _resultImgObjs[_result].SetActive(false);
+        _messageText.SetActive(false);
+    }
+
+    //遷移選択画面の表示
+    void TransSelecImgActive()
+    {
+        _transSelectionUI.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_nextStageButton);
     }
 
     public void ToNextStage()
